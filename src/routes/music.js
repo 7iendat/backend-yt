0dbf199983c314fda75a6cc584a875ae29b87657
Bùi Tấn Phát <103013376+krphat@users.noreply.github.com>
2024-04-21 21:39:55 +0700

    fix route music api

--- src/routes/music.js
+++ src/routes/music.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const musicController = require('../controllers/musicController')
 
-// router.route('/music').post(musicController.addNewMusic);
-// router.route('/musics/').get(musicController.getAllMusics);
-// router.route('/music/:id').get(musicController.getMusic);
-// router.route('/music/:id').put(musicController.updateMusic);
-// router.route('/music/:id').delete(musicController.deteteMuisc);
-
-router.post("/music",musicController.addNewMusic);
-router.get("/musics",musicController.getAllMusics);
-router.get("/music/get/:id",musicController.getMusic);
-router.put("/music/update/:id",musicController.updateMusic);
-router.delete("/music/delete/:id",musicController.deteteMuisc);
+router.route('/music').post(musicController.addNewMusic);
+router.route('/musics/').get(musicController.getAllMusics);
+router.route('/music/:id').get(musicController.getMusic);
+router.route('/music/:id').put(musicController.updateMusic);
+router.route('/music/:id').delete(musicController.deteteMuisc);
 
 module.exports = router;
